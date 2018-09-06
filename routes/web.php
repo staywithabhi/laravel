cf6a51afff43ac749130cc78bf46b5d8ecbc61b4
Abhishek dixit <abhishek@vortexglobal.in>
2018-09-06 17:28:05 +0530

    project added

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('dashboard', function () {
+    return view('dashboard');
+});
+Route::get('logins', function () {
+    return view('logins');
+});
+Route::get('/', function () {
+    //whatever
+})->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
