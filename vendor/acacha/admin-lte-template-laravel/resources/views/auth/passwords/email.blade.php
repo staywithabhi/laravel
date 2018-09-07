1b9cd90156175c0f1efad341246b13664d09072e
Abhishek dixit <abhishek@vortexglobal.in>
2018-09-07 15:57:50 +0530

    admin login added

--- vendor/acacha/admin-lte-template-laravel/resources/views/auth/passwords/email.blade.php
+++ vendor/acacha/admin-lte-template-laravel/resources/views/auth/passwords/email.blade.php
@@ -37,7 +37,6 @@
             <email-reset-password-form></email-reset-password-form>
 
             <a href="{{ url('/login') }}">Log in</a><br>
-            <a href="{{ url('/register') }}" class="text-center">{{ trans('adminlte_lang::message.registermember') }}</a>
 
         </div><!-- /.login-box-body -->
 
--- vendor/acacha/admin-lte-template-laravel/resources/views/layouts/partials/footer.blade.php
+++ vendor/acacha/admin-lte-template-laravel/resources/views/layouts/partials/footer.blade.php
@@ -2,7 +2,6 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-        <a href="#"></a><b>WestGate IT</b></a>. {{ trans('adminlte_lang::message.descriptionpackage') }}
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; 2017 <a href="#">WestGate IT</a>.</strong> 
