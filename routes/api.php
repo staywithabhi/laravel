9cd54a87672d18f0bbb499d0e0381fb28e62e063
Abhishek dixit <abhishek@vortexglobal.in>
2018-09-27 18:26:47 +0530

    remote added

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,6 @@ Route::group(['prefix' => 'v1','middleware' => 'auth:api'], function () {
     //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
     #adminlte_api_routes
 });
+
+Route::get('getClients',['as'=>'getClients','uses'=>'ClientController@index','middleware'=>'auth:api']);
+
