02f3a743c7e6a82371febeea7ffa1f38536d468e
Abhishek dixit <abhishek@vortexglobal.in>
2018-10-24 19:14:28 +0530

    tester

--- vendor/acacha/admin-lte-template-laravel/resources/views/auth/passwords/reset.blade.php
+++ vendor/acacha/admin-lte-template-laravel/resources/views/auth/passwords/reset.blade.php
@@ -10,49 +10,78 @@
 
     <div id="app">
         <div class="login-box">
-        <div class="login-logo">
-            <a href="{{ url('/home') }}">   <img src="{{ asset('/img/logo.png') }}"></a>
-        </div><!-- /.login-logo -->
+            <div class="login-logo">
+                <a href="{{ url('/home') }}">
+                    <img src="{{ asset('/img/logo.png') }}">
+                </a>
+            </div><!-- /.login-logo -->
 
-        @if (session('status'))
-            <div class="alert alert-success">
-                {{ session('status') }}
-            </div>
-        @endif
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
 
-        @if (count($errors) > 0)
-            <div class="alert alert-danger">
-                <strong>Whoops!</strong> {{ trans('adminlte_lang::message.someproblems') }}<br><br>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+            @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <strong>Whoops!</strong> {{ trans('adminlte_lang::message.someproblems') }}<br><br>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
 
-        <div class="login-box-body">
-            <p class="login-box-msg">{{ trans('adminlte_lang::message.passwordreset') }}</p>
+            <div class="login-box-body">
+                <p class="login-box-msg">{{ trans('adminlte_lang::message.passwordreset') }}</p>
+                <form action="{{ url('/password/reset') }}" method="post">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <input type="hidden" name="token" value="{{ $token }}">
+                    <div class="form-group has-feedback">
+                        <input type="email" class="form-control" placeholder="Email" name="email" value="{{ old('email') }}" autofocus required="required" />
+                        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                    </div>
 
-            <reset-password-form token="{{ $token }}">></reset-password-form>
+                    <div class="form-group has-feedback">
+                        <input type="password"  minlength="12" class="form-control" placeholder="Password" name="password" required="required"/>
+                        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                    </div>
 
-            <a href="{{ url('/login') }}">Log in</a><br>
+                    <div class="form-group has-feedback">
+                        <input type="password"  minlength="12" class="form-control" placeholder="Password" name="password_confirmation" required="required"/>
+                        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                    </div>
 
-        </div><!-- /.login-box-body -->
+                    <div class="row">
+                        <div class="col-xs-2">
+                        </div><!-- /.col -->
+                        <div class="col-xs-8">
+                            <button type="submit" class="btn btn-primary btn-block btn-flat">{{ trans('adminlte_lang::message.passwordreset') }}</button>
+                        </div><!-- /.col -->
+                        <div class="col-xs-2">
+                        </div><!-- /.col -->
+                    </div>
+                </form>
 
-    </div><!-- /.login-box -->
+                <a href="{{ url('/login') }}">Log in</a><br>
+<!--                 <a href="{{-- url('/register') --}}" class="text-center">{{-- trans('adminlte_lang::message.membership') --}}</a> -->
+
+            </div><!-- /.login-box-body -->
+
+        </div><!-- /.login-box -->
     </div>
 
     @include('adminlte::layouts.partials.scripts_auth')
 
     <script>
-        $(function () {
-            $('input').iCheck({
-                checkboxClass: 'icheckbox_square-blue',
-                radioClass: 'iradio_square-blue',
-                increaseArea: '20%' // optional
-            });
+      $(function () {
+        $('input').iCheck({
+          checkboxClass: 'icheckbox_square-blue',
+          radioClass: 'iradio_square-blue',
+          increaseArea: '20%' // optional
         });
+      });
     </script>
     </body>
 
